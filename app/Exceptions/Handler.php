b8b830d908c977584f5fdf23d1a9e1a8cc504ffc
sawaitomoya <swtmtaf1189@i.softbank.jp>
2022-01-12 18:49:08 +0900

    確認

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,15 +1,9 @@
 <?php
-
 namespace App\Exceptions;
 
 use Exception;
-<<<<<<< HEAD
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-=======
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
->>>>>>> ee3b45f27350e4dc8ecf9f19a533c9279578f0de
 
 class Handler extends ExceptionHandler
 {
@@ -21,7 +15,6 @@ class Handler extends ExceptionHandler
     protected $dontReport = [
         //
     ];
-
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -31,7 +24,6 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
-
     /**
      * Report or log an exception.
      *
@@ -42,7 +34,6 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
-
     /**
      * Render an exception into an HTTP response.
      *
@@ -50,23 +41,23 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-<<<<<<< HEAD
+
+     public function unauthenticated($request, AuthenticationException $exception)
+     {
+         if($request->expectsJson()){
+             return response()->json(['message' => $exception->getMessage()], 401);
+         }
+
+         if (in_array('admin', $exception->guards())) {
+             return redirect()->guest(route('admin.login'));
+         }
+
+         return redirect()->guest(route('login'));
+     }
+
+
     public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
-=======
-
-     protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if($request->expectsJson()){
-            return response()->json(['message' => $exception->getMessage()], 401);
-        }
-
-        if (in_array('admin', $exception->guards(), true)) {
-            return redirect()->guest(route('admin.login'));
-        }
-
-        return redirect()->guest(route('login'));
->>>>>>> ee3b45f27350e4dc8ecf9f19a533c9279578f0de
     }
 }
