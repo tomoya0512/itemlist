3c93d8b0b8ee99bd3f3ee68b4ce41623c6d7540d
sawaitomoya <swtmtaf1189@i.softbank.jp>
2022-01-12 22:10:14 +0900

    最新

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,18 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
-class HomeController extends Controller
+class AdminHomeController extends Controller
 {
-  public function __construct()
-  {
-      $this->middleware('auth:admin');
-  }
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
 
-  public function index()
-  {
-      return view('admin_home');
-  }
+    public function index()
+    {
+        return view('admin_home');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,32 +29,31 @@ Route::get('/', function () { return redirect('/home'); });
 | 2) User ログイン後
 |--------------------------------------------------------------------------
 */
-Route::group(['middleware' => 'auth:user'], function() {
-    Route::get('home','UserController@home');
-    Route::get('signup','UserController@signup');
-});
+// Route::group(['middleware' => 'auth:user'], function() {
+//     Route::get('home','UserController@home');
+//     Route::get('signup','UserController@signup');
+// });
 
 /*
 |--------------------------------------------------------------------------
 | 4) Admin ログイン後
 |--------------------------------------------------------------------------
 */
-route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'],function() {
-  Route::get('category/create','Admin\CategoryController@add');
-  Route::post('category/create','Admin\CategoryController@create');
-  Route::get('category', 'Admin\CategoryController@index');
-  Route::get('category/edit', 'Admin\CategoryController@edit');
-  Route::post('category/edit', 'Admin\CategoryController@update');
-  Route::get('category/delete', 'Admin\CategoryController@delete');
-  Route::get('item/create','Admin\ItemController@add');
-  Route::post('item/create','Admin\ItemController@create');
-  Route::get('item', 'Admin\ItemController@index');
-  Route::get('item/edit', 'Admin\ItemController@edit');
-  Route::post('item/edit', 'Admin\ItemController@update');
-  Route::get('item/delete', 'Admin\ItemController@delete');
-
-});
-
+// route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'],function() {
+//   Route::get('category/create','Admin\CategoryController@add');
+//   Route::post('category/create','Admin\CategoryController@create');
+//   Route::get('category', 'Admin\CategoryController@index');
+//   Route::get('category/edit', 'Admin\CategoryController@edit');
+//   Route::post('category/edit', 'Admin\CategoryController@update');
+//   Route::get('category/delete', 'Admin\CategoryController@delete');
+//   Route::get('item/create','Admin\ItemController@add');
+//   Route::post('item/create','Admin\ItemController@create');
+//   Route::get('item', 'Admin\ItemController@index');
+//   Route::get('item/edit', 'Admin\ItemController@edit');
+//   Route::post('item/edit', 'Admin\ItemController@update');
+//   Route::get('item/delete', 'Admin\ItemController@delete');
+//
+// });
 Auth::routes();
 
 Route::group(['prefix' => 'admin'], function(){
